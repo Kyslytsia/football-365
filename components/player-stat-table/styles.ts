23166de2413208205c759aa3b3cb3715755e7287
Denys Kyslytsia <kislica779@gmail.com>
2024-08-09 09:39:18 +0300

    Changed styles in PlayerStatTable

--- components/player-stat-table/styles.ts
+++ components/player-stat-table/styles.ts
@@ -1,5 +1,5 @@
 export const getStyles = () => ({
-  wrapper: "text-white text-[18px] font-medium",
+  wrapper: "flex items-center gap-y-2 w-1/3 text-white text-[18px] font-medium",
   whiteText: "text-white text-[18px] font-medium",
   greyText: "text-Grey text-[12px]",
 });
