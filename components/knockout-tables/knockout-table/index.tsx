9285768c7e6298fa353899c3ee383da52babf9c2
Denys Kyslytsia <kislica779@gmail.com>
2025-10-03 11:28:13 +0300

    Changed table knockout lines

--- components/knockout-tables/knockout-table/index.tsx
+++ components/knockout-tables/knockout-table/index.tsx
@@ -4,11 +4,7 @@ import { ScrollView, View } from "react-native";
 import { KnockoutTableProps } from "./types";
 import { KnockoutMatch } from "../../knockout-match";
 import { KnockoutMatches } from "../../knockout-matches";
-import {
-  roundOf16Lines,
-  quarterFinalsLines,
-  semiFinalsAndFinalLines,
-} from "./lineClasses";
+import { roundOf16Lines, quarterFinalsLines } from "./lineClasses";
 
 export const KnockoutTable = memo(
   ({ isNational, ...props }: KnockoutTableProps) => {
@@ -21,93 +17,94 @@ export const KnockoutTable = memo(
 
     return (
       <ScrollView>
-        <View className="relative gap-y-11 py-5 mx-auto w-[360px]">
+        <View className="relative">
           {renderLines(roundOf16Lines)}
           {renderLines(quarterFinalsLines)}
-          {renderLines(semiFinalsAndFinalLines)}
 
-          <View className={`${roundWrapperStyle} w-[320px]`}>
-            {isNational ? (
-              <>
-                <KnockoutMatch match={props.roundOf16_1} />
-                <KnockoutMatch match={props.roundOf16_2} />
-                <KnockoutMatch match={props.roundOf16_3} />
-                <KnockoutMatch match={props.roundOf16_4} />
-              </>
-            ) : (
-              <>
-                <KnockoutMatches matches={props.roundOf16_1} />
-                <KnockoutMatches matches={props.roundOf16_2} />
-                <KnockoutMatches matches={props.roundOf16_3} />
-                <KnockoutMatches matches={props.roundOf16_4} />
-              </>
-            )}
-          </View>
+          <View className="relative gap-y-12 py-5 mx-auto w-[360px] z-10">
+            <View className={`${roundWrapperStyle} w-[320px]`}>
+              {isNational ? (
+                <>
+                  <KnockoutMatch match={props.roundOf16_1} />
+                  <KnockoutMatch match={props.roundOf16_2} />
+                  <KnockoutMatch match={props.roundOf16_3} />
+                  <KnockoutMatch match={props.roundOf16_4} />
+                </>
+              ) : (
+                <>
+                  <KnockoutMatches matches={props.roundOf16_1} />
+                  <KnockoutMatches matches={props.roundOf16_2} />
+                  <KnockoutMatches matches={props.roundOf16_3} />
+                  <KnockoutMatches matches={props.roundOf16_4} />
+                </>
+              )}
+            </View>
 
-          <View className={`${roundWrapperStyle} w-[233px]`}>
-            {isNational ? (
-              <>
-                <KnockoutMatch match={props.quarterFinals_1} />
-                <KnockoutMatch match={props.quarterFinals_2} />
-              </>
-            ) : (
-              <>
-                <KnockoutMatches matches={props.quarterFinals_1} />
-                <KnockoutMatches matches={props.quarterFinals_2} />
-              </>
-            )}
-          </View>
+            <View className={`${roundWrapperStyle} w-[235px]`}>
+              {isNational ? (
+                <>
+                  <KnockoutMatch match={props.quarterFinals_1} />
+                  <KnockoutMatch match={props.quarterFinals_2} />
+                </>
+              ) : (
+                <>
+                  <KnockoutMatches matches={props.quarterFinals_1} />
+                  <KnockoutMatches matches={props.quarterFinals_2} />
+                </>
+              )}
+            </View>
 
-          <View className="flex-row mx-auto">
-            {isNational ? (
-              <KnockoutMatch match={props.semiFinals_1} />
-            ) : (
-              <KnockoutMatches matches={props.semiFinals_1} />
-            )}
-          </View>
+            <View className="flex-row mx-auto">
+              {isNational ? (
+                <KnockoutMatch match={props.semiFinals_1} />
+              ) : (
+                <KnockoutMatches matches={props.semiFinals_1} />
+              )}
+            </View>
 
-          <View className="flex-row mx-auto">
-            <KnockoutMatch isFinal match={props.final} />
-          </View>
+            <View className="flex-row mx-auto">
+              <KnockoutMatch isFinal match={props.final} />
+            </View>
 
-          <View className="flex-row mx-auto">
-            {isNational ? (
-              <KnockoutMatch match={props.semiFinals_2} />
-            ) : (
-              <KnockoutMatches matches={props.semiFinals_2} />
-            )}
-          </View>
+            <View className="flex-row mx-auto">
+              {isNational ? (
+                <KnockoutMatch match={props.semiFinals_2} />
+              ) : (
+                <KnockoutMatches matches={props.semiFinals_2} />
+              )}
+            </View>
 
-          <View className={`${roundWrapperStyle} w-[233px]`}>
-            {isNational ? (
-              <>
-                <KnockoutMatch match={props.quarterFinals_3} />
-                <KnockoutMatch match={props.quarterFinals_4} />
-              </>
-            ) : (
-              <>
-                <KnockoutMatches matches={props.quarterFinals_3} />
-                <KnockoutMatches matches={props.quarterFinals_4} />
-              </>
-            )}
-          </View>
+            <View className={`${roundWrapperStyle} w-[235px]`}>
+              {isNational ? (
+                <>
+                  <KnockoutMatch match={props.quarterFinals_3} />
+                  <KnockoutMatch match={props.quarterFinals_4} />
+                </>
+              ) : (
+                <>
+                  <KnockoutMatches matches={props.quarterFinals_3} />
+                  <KnockoutMatches matches={props.quarterFinals_4} />
+                </>
+              )}
+            </View>
 
-          <View className={`${roundWrapperStyle} w-[320px]`}>
-            {isNational ? (
-              <>
-                <KnockoutMatch match={props.roundOf16_5} />
-                <KnockoutMatch match={props.roundOf16_6} />
-                <KnockoutMatch match={props.roundOf16_7} />
-                <KnockoutMatch match={props.roundOf16_8} />
-              </>
-            ) : (
-              <>
-                <KnockoutMatches matches={props.roundOf16_5} />
-                <KnockoutMatches matches={props.roundOf16_6} />
-                <KnockoutMatches matches={props.roundOf16_7} />
-                <KnockoutMatches matches={props.roundOf16_8} />
-              </>
-            )}
+            <View className={`${roundWrapperStyle} w-[320px]`}>
+              {isNational ? (
+                <>
+                  <KnockoutMatch match={props.roundOf16_5} />
+                  <KnockoutMatch match={props.roundOf16_6} />
+                  <KnockoutMatch match={props.roundOf16_7} />
+                  <KnockoutMatch match={props.roundOf16_8} />
+                </>
+              ) : (
+                <>
+                  <KnockoutMatches matches={props.roundOf16_5} />
+                  <KnockoutMatches matches={props.roundOf16_6} />
+                  <KnockoutMatches matches={props.roundOf16_7} />
+                  <KnockoutMatches matches={props.roundOf16_8} />
+                </>
+              )}
+            </View>
           </View>
         </View>
       </ScrollView>
--- components/knockout-tables/knockout-table/lineClasses.ts
+++ components/knockout-tables/knockout-table/lineClasses.ts
@@ -1,112 +1,85 @@
 export const roundOf16Lines = [
   {
     className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { top: 88, left: "15%" },
+    style: { top: 88, left: "18%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { top: 88, left: "38.7%" },
+    style: { top: 88, left: "39%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { top: 88, left: "60.7%" },
+    style: { top: 88, left: "60.1%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { top: 88, left: "84.2%" },
+    style: { top: 88, left: "81.5%" },
   },
   {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { top: 110, left: "27%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { top: 110, left: 54, width: "24.2%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { top: 110, right: "27%" },
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[109px]",
+    style: { top: 110, left: "28.5%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { top: 110, right: 55, width: "23.9%" },
+    style: { top: 110, left: 72, width: "21%" },
   },
   {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 90, left: "15%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 90, left: "38.9%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 90, left: "60.5%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 90, left: "84.5%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[22px]",
-    style: { bottom: 110, left: "27%" },
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[109px]",
+    style: { top: 110, right: "28.5%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { bottom: 110, left: 54, width: "24.5%" },
+    style: { top: 110, right: 72, width: "21%" },
   },
   {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[22px]",
-    style: { bottom: 110, right: "27%" },
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
+    style: { bottom: 88, left: "18%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
+    style: { bottom: 88, left: "39%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
+    style: { bottom: 88, left: "60.1%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
+    style: { bottom: 88, left: "81.5%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[110px]",
+    style: { bottom: 110, left: "28.5%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { bottom: 110, right: 54, width: "24.5%" },
+    style: { bottom: 110, left: 72, width: "21%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[110px]",
+    style: { bottom: 110, right: "28.5%" },
+  },
+  {
+    className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
+    style: { bottom: 110, right: 72, width: "21%" },
   },
 ];
 
 export const quarterFinalsLines = [
   {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { top: 199, left: "27%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { top: 219, left: "49.5%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { top: 199, left: "72.5%" },
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[140px]",
+    style: { top: 219, left: "50%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { top: 217, left: 98, width: "45.5%" },
+    style: { top: 217, left: 114, width: "42%" },
   },
   {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 201, left: "27%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[24px]",
-    style: { bottom: 220, left: "49.5%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[20px]",
-    style: { bottom: 201, left: "72.5%" },
+    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[140px]",
+    style: { bottom: 220, left: "50%" },
   },
   {
     className: "absolute bg-[#a6b8c9] z-[1] h-[2px]",
-    style: { bottom: 219, left: 98, width: "45.5%" },
-  },
-];
-
-export const semiFinalsAndFinalLines = [
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[44px]",
-    style: { top: 312, left: "49.5%" },
-  },
-  {
-    className: "absolute bg-[#a6b8c9] z-[1] w-[2px] h-[43px]",
-    style: { bottom: 314, left: "49.5%" },
+    style: { bottom: 219, left: 112, width: "43%" },
   },
 ];
