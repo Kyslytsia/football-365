8c21d312d3c86ce3e3b52072adb283f0732e271f
Denys Kyslytsia <kislica779@gmail.com>
2025-10-03 09:38:34 +0300

    New changed 2

--- components/table/statistics-table/index.tsx
+++ components/table/statistics-table/index.tsx
@@ -54,13 +54,8 @@ export const StatisticsTable = memo(
       .minDistance(20)
       .onUpdate((e) => {
         offset.value = Math.min(
-<<<<<<< Updated upstream
-          Math.max(e.translationX + startOffset.value, -400),
-          -210
-=======
           Math.max(e.translationX + startOffset.value, -380),
           -205
->>>>>>> Stashed changes
         );
       })
       .onEnd(() => {
